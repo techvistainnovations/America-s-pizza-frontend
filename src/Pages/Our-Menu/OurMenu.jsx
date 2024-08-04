922ca64d839e3895bb55e9765ab61aa2d57a4497
jagdishseervi775 <jagdishseervi775@gmail.com>
2024-08-05 00:11:36 +0530

    responsive

--- src/Pages/Our-Menu/OurMenu.jsx
+++ src/Pages/Our-Menu/OurMenu.jsx
@@ -214,13 +214,13 @@ const OurMenu = () => {
   return (
     <div className='mt-[148px]'>
       <section className='container pt-3 mx-auto mt-[142px]  lg:mb-12 md:mb-6 sm:mb-4 mb-4'>
-        <h2 className='lg:text-4xl md:text-3xl text-2xl text-darkbg font-semibold md:leading-[65px] text-center sm:leading-10'>Explore Menu</h2>
-        <div className='grid items-stretch gap-6 grid-cols-3'>
+        <h2 className='lg:text-4xl mb-6 md:text-3xl text-2xl text-darkbg font-semibold md:leading-[65px] text-center sm:leading-10'>Explore Menu</h2>
+        <div className='grid items-stretch gap-6 lg:grid-cols-3 md:grid-cols-2'>
           {
             menuItems.map((menuItem, i) => {
               return (
                 <div key={menuItem.id} onClick={handleClickOpen} className='cursor-pointer rounded-2xl hover:drop-shadow-lg transition-all menu_item shadow-sm'>
-                  <div className='relative h-60 overflow-hidden transition-all rounded-t-lg'>
+                  <div className='relative lg:h-60 h-44 overflow-hidden transition-all rounded-t-lg'>
                     <img src={menuItem.coverImage} className='w-full h-full transition-all object-cover' alt="" />
                     <h2 className='absolute transition-all bottom-3 z-20 left-0 pr-36 p-4 text-whiteColor w-full text-4xl'>{menuItem.title}</h2>
                     <div className='py-1 text-sm font-semibold rounded-full px-3 absolute bottom-8 right-3 z-20 text-whiteColor bg-[#DA251C]'>{menuItem.items.length - 1}+ Types</div>
